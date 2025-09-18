c3f23ee5f47637f93984f23d2d4ffecf8e2dd729
Oleg Somov <swinkin55@yandex.ru>
2025-09-18 15:56:57 +0300

    add gemma

--- make_intervention.py
+++ make_intervention.py
@@ -17,6 +17,8 @@ model_name2simple_model_name = {
         "tiiuae/Falcon3-7B-Instruct": "falcon3-7B",
         "alpindale/Llama-3.2-3B-Instruct": "llama32-3B",
         "alpindale/Llama-3.2-1B-Instruct": "llama32-1B",
+        "google/gemma-2-2b-it": "gemma2-2B",
+        "google/gemma-2-9b-it": "gemma2-9B",
     }
 
 
