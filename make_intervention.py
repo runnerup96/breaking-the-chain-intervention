c14338f2353fc7529dc3cd9f171b7c00fe5ce867
Seleznev Mikhail <seleznev@airi.net>
2025-09-19 11:49:52 +0300

    Add paraphrases to main script

--- make_intervention.py
+++ make_intervention.py
@@ -57,9 +57,10 @@ if __name__ == "__main__":
         assert len(few_shot_examples) == 5
 
         dataset_path = os.path.join(project_path, "entailment_trees_emnlp2021_data_v3/dataset/task_2/test.jsonl")
-        dataset = entailment_dataset.EntailmentDataset(dataset_path)
+        paraphrases_path = os.path.join(project_path, "entailment_trees_emnlp2021_data_v3/dataset/task_2/aligned_test_question_paraphases.json")
+        dataset = entailment_dataset.EntailmentDataset(dataset_path, paraphrases_path)
         dataloader = DataLoader(dataset, batch_size=args.batch_size, collate_fn=lambda batch: batch, shuffle=False)
-        intervention_logic = entailment_intervention.EntailmentIntervention(dataset, few_shot_examples=few_shot_examples)
+        intervention_logic = entailment_intervention.EntailmentIntervention(dataset, few_shot_examples=few_shot_examples, hsvt_mode="paraphrase")
         evaluator = entailment_evaluation.EntailmentEvaluation(dataset, intervention_logic)
     elif args.evaluation_dataset == "averitec":
         dataset_path = os.path.join(project_path, "statics/result_splits/AVeriTeC/data")
