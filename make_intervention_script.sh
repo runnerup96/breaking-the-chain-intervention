d7c7f81d135c14638a757486bf510997c1545740
Seleznev Mikhail <seleznev@airi.net>
2025-09-24 12:59:15 +0300

    Update overall script for final experiments

--- make_intervention_script.sh
+++ make_intervention_script.sh
@@ -27,19 +27,22 @@ get_batch_size() {
     esac
 }
 
-project_path="/home/somov/frontdoor_llm_causality"
-CUDA_DEVICE_NUMBER=0
+project_path="/mnt/extremessd10tb/seleznev/breaking-the-chain-intervention"
+CUDA_DEVICE_NUMBER=1
 
-datasets=("ricechem" "averitec")
+datasets=("entailment")
 
 models=(
+    # "Qwen/Qwen3-1.7B"
     "Qwen/Qwen3-4B"
-    "tiiuae/Falcon3-3B-Instruct"
-#    "tiiuae/Falcon3-7B-Instruct"
-#    "alpindale/Llama-3.2-3B-Instruct"
-    "alpindale/Llama-3.2-1B-Instruct"
-    "google/gemma-2-2b-it"
-#    "google/gemma-2-9b-it"
+    # "Qwen/Qwen3-8B"
+    # "unsloth/Meta-Llama-3.1-8B-Instruct"
+    # "tiiuae/Falcon3-3B-Instruct"
+    # "tiiuae/Falcon3-7B-Instruct"
+    # "alpindale/Llama-3.2-3B-Instruct"
+    # "alpindale/Llama-3.2-1B-Instruct"
+    # "google/gemma-2-2b-it"
+    # "google/gemma-2-9b-it"
 )
 
 run_name="intervention_batch_$(date +%Y%m%d_%H%M%S)"
@@ -55,8 +58,7 @@ for model_name in "${models[@]}"; do
         simple_model_name=$(model_name2simple_model_name "$model_name")
         
         echo "Running: $simple_model_name on $evaluation_dataset with batch_size=$batch_size"
-
-        tmux send-keys "PROJECT_PATH='${project_path}' CUDA_VISIBLE_DEVICES='$CUDA_DEVICE_NUMBER' /home/somov/miniconda3/envs/llm_tuning/bin/python make_intervention.py \
+        tmux send-keys "PROJECT_PATH='${project_path}' CUDA_VISIBLE_DEVICES='$CUDA_DEVICE_NUMBER' uv run make_intervention.py \
             --model_name '$model_name' \
             --evaluation_dataset '$evaluation_dataset' \
             --batch_size $batch_size" ENTER
