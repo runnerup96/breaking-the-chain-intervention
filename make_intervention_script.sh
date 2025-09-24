182f3906d810882739d6b77ed9f25c9dfb798a0c
Oleg Somov <swinkin55@yandex.ru>
2025-09-24 15:35:11 +0300

    add entailment

--- make_intervention_script.sh
+++ make_intervention_script.sh
@@ -2,7 +2,7 @@
 
 # make sure to do
 # sudo chmod -R 755 intervention_predictions/{evaluation_dataset}
-# sudo chown -R somov intervention_predictions/{evaluation_dataset}
+# sudo chown -R user_name intervention_predictions/{evaluation_dataset}
 
 model_name2simple_model_name() {
     echo "${1##*/}"
@@ -27,25 +27,26 @@ get_batch_size() {
     esac
 }
 
-project_path="/mnt/extremessd10tb/seleznev/breaking-the-chain-intervention"
-CUDA_DEVICE_NUMBER=1
+project_path=""
+CUDA_DEVICE_NUMBER=0
 
-datasets=("entailment")
+datasets=("averitec" "ricechem" "entailment")
 
 models=(
-    # "Qwen/Qwen3-1.7B"
+    "Qwen/Qwen3-1.7B"
     "Qwen/Qwen3-4B"
-    # "Qwen/Qwen3-8B"
-    # "unsloth/Meta-Llama-3.1-8B-Instruct"
-    # "tiiuae/Falcon3-3B-Instruct"
-    # "tiiuae/Falcon3-7B-Instruct"
-    # "alpindale/Llama-3.2-3B-Instruct"
-    # "alpindale/Llama-3.2-1B-Instruct"
-    # "google/gemma-2-2b-it"
-    # "google/gemma-2-9b-it"
+    "Qwen/Qwen3-8B"
+    "tiiuae/Falcon3-3B-Instruct"
+    "tiiuae/Falcon3-7B-Instruct"
+    "alpindale/Llama-3.2-3B-Instruct"
+    "alpindale/Llama-3.2-1B-Instruct"
+    "unsloth/Meta-Llama-3.1-8B-Instruct"
+    "google/gemma-2-2b-it"
+    "google/gemma-2-9b-it"
 )
 
 run_name="intervention_batch_$(date +%Y%m%d_%H%M%S)"
+python_path=""
 tmux new-session -d -s $run_name
 
 echo "Starting intervention runs in tmux session: $run_name"
@@ -56,9 +57,10 @@ for model_name in "${models[@]}"; do
     for evaluation_dataset in "${datasets[@]}"; do
         batch_size=$(get_batch_size "$model_name")
         simple_model_name=$(model_name2simple_model_name "$model_name")
-        
+
         echo "Running: $simple_model_name on $evaluation_dataset with batch_size=$batch_size"
-        tmux send-keys "PROJECT_PATH='${project_path}' CUDA_VISIBLE_DEVICES='$CUDA_DEVICE_NUMBER' uv run make_intervention.py \
+
+        tmux send-keys "PROJECT_PATH='${project_path}' CUDA_VISIBLE_DEVICES='$CUDA_DEVICE_NUMBER' '$python_path' \
             --model_name '$model_name' \
             --evaluation_dataset '$evaluation_dataset' \
             --batch_size $batch_size" ENTER
