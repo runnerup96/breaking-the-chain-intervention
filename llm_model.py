c7af630205b8bf60605019a3af2746350a8d18aa
Seleznev Mikhail <seleznev@airi.net>
2025-11-17 15:56:30 +0300

    Minor naming fix due to torch versions

--- llm_model.py
+++ llm_model.py
@@ -28,7 +28,7 @@ class LLMModel:
         self.model = AutoModelForCausalLM.from_pretrained(
             model_name,
             device_map=device_map,
-            dtype=dtype
+            torch_dtype=dtype
         )
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.tokenizer.padding_side = 'left'
