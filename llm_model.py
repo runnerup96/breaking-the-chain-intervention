f1e1915cd6824b57c8725b0789d7849d4e9fc1db
Mikhail <mishac22@yandex.ru>
2025-11-24 15:27:44 +0300

    added api logic

--- llm_model.py
+++ llm_model.py
@@ -2,16 +2,27 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from typing import Dict, List, Union
 import re
+import os
+
+from openai import OpenAI
+from openai import DefaultHttpxClient
 
 
 QWEN3_MODEL_FAMILY = "Qwen3"
 FALCON3_MODEL_FAMILY = "Falcon3"
 LLAMA32_MODEL_FAMILY = "Llama3.2"
+LLAMA31_MODEL_FAMILY = "Llama3.1"
 GEMMA2_MODEL_FAMILY = "Gemma2"
-
+GPT_MODEL_FAMILY = "GPT"
 
 class LLMModel:
-    def __init__(self, model_name: str, device_map: str = "auto", torch_dtype: torch.dtype = torch.bfloat16):
+    def __init__(self, model_name: str,
+                 device_map: str = "auto",
+                 torch_dtype: torch.dtype = torch.bfloat16,
+                 use_api: bool = False,
+                 api_base_url: str | None = None,
+                 tokenizer_name: str | None = None,
+                 ):
         """
         Initialize the LLM model.
         
@@ -24,25 +35,53 @@ class LLMModel:
         self.device_map = device_map
         self.torch_dtype = torch_dtype
 
-        self.model = AutoModelForCausalLM.from_pretrained(
-            model_name,
-            device_map=device_map,
-            torch_dtype=torch_dtype
-        )
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.use_api = use_api
+        self.api_base_url = api_base_url
+
+        self.tokenizer_name = tokenizer_name if tokenizer_name else model_name
+
+        self.tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_name)
         self.tokenizer.padding_side = 'left'
         if self.tokenizer.pad_token == None:
             self.tokenizer.pad_token = self.tokenizer.eos_token
-        self.model.generation_config.pad_token_id = self.tokenizer.pad_token_id
+
+        if not self.use_api:
+            self.model = AutoModelForCausalLM.from_pretrained(
+                model_name,
+                device_map=device_map,
+                torch_dtype=torch_dtype
+            )
+            
+            self.model.generation_config.pad_token_id = self.tokenizer.pad_token_id
+        else:
+            if api_base_url is None:
+                raise ValueError("api_base_url must be provided when use_api=True")
+            
+            api_key = os.getenv("OPENAI_KEY")
+            if api_key is None:
+                raise ValueError(
+                    "Environment variable OPENAI_KEY is not set, "
+                    "but use_api=True. Please export OPENAI_KEY."
+                )
+            
+            self.client = OpenAI(
+                base_url=api_base_url,
+                api_key=api_key,
+                http_client=DefaultHttpxClient(verify=False)
+            )
 
         self.model_family = self.identify_model_family(model_name)
         
-        print(f"Model {model_name} initialized")
+        print("Local" if not self.use_api else "API", f"model {model_name} initialized")
 
     def identify_model_family(self, model_name: str) -> str:
         """
         Identify the type of the model.
         """
+        if self.use_api:
+            if 'gpt' in self.model_name.lower():
+                return GPT_MODEL_FAMILY
+
         if (hasattr(self.model.config, 'architectures') and 
                          self.model.config.architectures and 
                          self.model.config.architectures[0] == "Qwen3ForCausalLM"):
@@ -59,6 +98,11 @@ class LLMModel:
             return LLAMA32_MODEL_FAMILY
         elif (hasattr(self.model.config, 'architectures') and 
               self.model.config.architectures and 
+              self.model.config.architectures[0] == "LlamaForCausalLM" and
+              "llama-3.1" in model_name.lower()):
+            return LLAMA31_MODEL_FAMILY
+        elif (hasattr(self.model.config, 'architectures') and 
+              self.model.config.architectures and 
               self.model.config.architectures[0] == "Gemma2ForCausalLM"):
             return GEMMA2_MODEL_FAMILY
         else:
@@ -66,12 +110,15 @@ class LLMModel:
     
     def generate(self, prompts:  List[str], max_new_tokens: int,
                  skip_special_tokens: bool) -> List[Dict[str, str]]:
+        if self.use_api:
+            return self._generate_api_batch(prompts, max_new_tokens, skip_special_tokens)
+        
         if self.model_family == QWEN3_MODEL_FAMILY:
             return self._generate_qwen3_batch(prompts, max_new_tokens, skip_special_tokens)
         elif self.model_family == FALCON3_MODEL_FAMILY:
             return self._generate_falcon3_batch(prompts, max_new_tokens, skip_special_tokens)
-        elif self.model_family == LLAMA32_MODEL_FAMILY:
-            return self._generate_llama32_batch(prompts, max_new_tokens, skip_special_tokens)
+        elif self.model_family in [LLAMA32_MODEL_FAMILY, LLAMA31_MODEL_FAMILY]:
+            return self._generate_llama_batch(prompts, max_new_tokens, skip_special_tokens)
         elif self.model_family == GEMMA2_MODEL_FAMILY:
             return self._generate_gemma2_batch(prompts, max_new_tokens, skip_special_tokens)
         else:
@@ -88,19 +135,7 @@ class LLMModel:
                 add_generation_prompt=add_generation_prompt,
                 enable_thinking=False
             )
-        elif self.model_family == FALCON3_MODEL_FAMILY:
-            prompt = self.tokenizer.apply_chat_template(
-                messages,
-                tokenize=False,
-                add_generation_prompt=add_generation_prompt,
-            )
-        elif self.model_family == LLAMA32_MODEL_FAMILY:
-            prompt = self.tokenizer.apply_chat_template(
-                messages,
-                tokenize=False,
-                add_generation_prompt=add_generation_prompt,
-            )
-        elif self.model_family == GEMMA2_MODEL_FAMILY:
+        elif self.model_family in [FALCON3_MODEL_FAMILY, LLAMA32_MODEL_FAMILY, LLAMA31_MODEL_FAMILY, GEMMA2_MODEL_FAMILY, GPT_MODEL_FAMILY]:
             prompt = self.tokenizer.apply_chat_template(
                 messages,
                 tokenize=False,
@@ -163,7 +198,7 @@ class LLMModel:
         
         return results
 
-    def _generate_llama32_batch(self, prompts: List[str], max_new_tokens: int,
+    def _generate_llama_batch(self, prompts: List[str], max_new_tokens: int,
                                skip_special_tokens: bool) -> List[Dict[str, str]]:
         """
         Generate text for multiple prompts using Llama3.2 model in batch.
@@ -218,6 +253,33 @@ class LLMModel:
             results.append({"prompt": prompt, "completion": completion})
         
         return results
+    
+    def _generate_api_batch(
+        self,
+        prompts: List[str],
+        max_new_tokens: int,
+        skip_special_tokens: bool) -> List[Dict[str, str]]:
+
+        resp = self.client.completions.create(
+            model=self.model_name,
+            prompt=prompts,
+            max_tokens=max_new_tokens,
+            temperature=0.0,
+        )
+
+        index2choice = {c.index: c for c in resp.choices}
+
+        results = []
+        for i, prompt in enumerate(prompts):
+            choice = index2choice[i]
+            completion_text = choice.text
+
+            results.append({
+                "prompt": prompt,
+                "completion": completion_text,
+            })
+
+        return results
 
     def clean_model_specific_completion(self, output: str) -> str:
         if self.model_family == QWEN3_MODEL_FAMILY:
@@ -227,12 +289,16 @@ class LLMModel:
             output = re.sub(r'<\|endoftext\|>|<\|im_end\|>', '', output)
         elif self.model_family == FALCON3_MODEL_FAMILY:
             output = re.sub(r'<\|endoftext\|>', '', output)
-        elif self.model_family == LLAMA32_MODEL_FAMILY:
+        elif self.model_family in [LLAMA32_MODEL_FAMILY, LLAMA31_MODEL_FAMILY]:
             output = re.sub(r'<\|eot_id\|>', '', output)
         elif self.model_family == GEMMA2_MODEL_FAMILY:
             last_end_of_turn = output.rfind('<end_of_turn>')
             if last_end_of_turn != -1:
                 output = output[:last_end_of_turn]
+        elif self.model_family == GPT_MODEL_FAMILY:
+            last_return = output.rfind('<|return|>')
+            if last_return != -1:
+                output = output[:last_return]
         else:
             raise NotImplementedError(f"Model family for {self.model_name} not yet implemented")
         return output
